1562b81d9fed691542ec6f5758f38cbba98f86db
Víctor Illanes <villanes9@gmail.com>
2022-03-17 12:33:03 -0300

    Se crea un React.useEffect

--- manejoproestado/src/Components/UseState/index.js
+++ manejoproestado/src/Components/UseState/index.js
@@ -9,13 +9,39 @@ function UseState({name}){
    const [loading,setLoading]=React.useState(false);
    //Se crea el estado
 
+
+   React.useEffect(()=>{
+
+        console.log(loading);
+        console.log("Empezando efecto useEffect");
+
+
+        if (!!loading){ //Si Loading es True
+
+
+            setTimeout(()=>{
+
+                 console.log("Consulta la BackEnd");
+
+                 console.log("Muestra de datos desde BackEnd");
+
+            },3000); //Se espera 5 seg para ejecutarse
+        }
+
+
+        console.log("Terminando el efecto useEffect");
+
+
+
+   },[loading]) //Se ejecuta solo cuando cambia el estado de Loading
+
     return (
         <div>
             <h2>Eliminar {name}</h2>
             <p>Por favor, escribe el codigo de seguridad.</p>
             {error && (<p>Error: El código es incorrecto</p>)}
             {loading && (<p>Loading...</p>)}
-            <div><input placeholder="Codigo de seguridad"/><button onClick={()=>setLoading(!error)}>Comprobar</button></div> 
+            <div><input placeholder="Codigo de seguridad"/><button onClick={()=>setLoading(!loading)}>Comprobar</button></div> 
             
         </div>
     );  
